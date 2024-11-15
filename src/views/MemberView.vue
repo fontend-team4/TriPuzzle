18d16d6e087abff0d8dd199abeecb746b8e819b6
Emma Huang <huiwenhuang1998@gmail.com>
2024-11-15 23:23:08 +0800

    first commit

---
+++ src/views/MemberView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>This is an member page</h1>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/views/PlannerView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    MapComponent + SideBar + MainComponent
+  </div>
+</template>
