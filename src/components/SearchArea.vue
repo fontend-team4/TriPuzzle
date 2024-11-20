4a1d17e4f275faa8c0f90c8aae55467329a97e13
Rod Chang <a0942248868@gmail.com>
2024-11-21 00:11:31 +0800

    新增彈窗式頁面

--- src/components/SearchArea.vue
+++ src/components/SearchArea.vue
@@ -6,6 +6,18 @@ import{ ref } from "vue"
 
 <template>
   <div class="search">
+    
+  <button>
+    <span class="text-gray">選擇區域</span>
+    <!-- <span> {{ selectArea.name }}</span> 預留功能-->
+    <ChevronDownIcon class="Icon-SelectArea text-primary-800"/>
+  </button>
+    
+  <button>
+    <span class="text-gray">選擇區域</span>
+    <!-- <span> {{ selectArea.name }}</span> 預留功能-->
+    <ChevronDownIcon class="Icon-SelectArea text-primary-800"/>
+  </button>
   </div>
 </template>
 
