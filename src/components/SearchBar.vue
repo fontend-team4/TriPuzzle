bf6bd2fb12ec135c7f865c67754e47c57275916d
Rod Chang <a0942248868@gmail.com>
2024-11-20 15:52:20 +0800

    檢查顏色、移除無用標籤

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,27 +7,27 @@ import SearchArea from './SearchArea.vue';
 <template>
   <!-- 區塊-搜尋列 -->
   <div class="row-search">
-    <div class="container-search noscroll searchbar">
+    <div class="container-search searchbar">
       <!-- 搜尋列-分類模式 -->
       <div class="search-category">
         <!-- 選單-選擇區域 -->
         <div class="block-area ">
           <div class="btn-area bg-primary-600">
-            <span>選擇區域</span>
+            <span class="text-gray">選擇區域</span>
             <!-- <span> {{ selectArea.name }}</span> 預留功能-->
             <ChevronDownIcon class="Icon-SelectArea text-primary-800"/>
           </div>
         </div>
         <!-- 搜尋列 -->
-        <div class="block-search-mask hidden-md-and-down">
+        <div class="block-search-mask">
           <MagnifyingGlassIcon class="Icon-MagnifyingGlass" />
           <span>搜尋...</span>
         </div>
         <!-- 分類選單 -->
         <div class="block-category">
-          <ul class="category-list text-primary-800">
+          <ul class="category-list">
             <li class="category-item active">景點</li>
-            <li class="category-item col-primary-800">美食</li>
+            <li class="category-item">美食</li>
             <li class="category-item">購物</li>
             <li class="category-item">住宿</li>
             <li class="category-item">租車站</li>
@@ -38,8 +38,8 @@ import SearchArea from './SearchArea.vue';
         </div>
         <!-- 篩選 -->
         <div class="block-filter">
-          <button data-v-filter type="button" class="filter-btn">
-            <AdjustmentsHorizontalIcon data-v-filter class="Icon-Filter icon" />
+          <button type="button" class="filter-btn">
+            <AdjustmentsHorizontalIcon  class="Icon-Filter icon text-black" />
           </button>
         </div>
       </div>
@@ -54,7 +54,7 @@ import SearchArea from './SearchArea.vue';
                 <input class="el-input_inner" type="text" autocomplete="off" tabindex="0" placeholder="輸入關鍵字" id="">
               </div>
             </div>
-            <XCircleIcon class="Icon-clear"  aria-hidden="true" />
+            <XCircleIcon class="Icon-clear" />
           </div>
         </div>
       </div>
@@ -72,7 +72,7 @@ import SearchArea from './SearchArea.vue';
     display: block;
   }
 
-  .row-search[data-v-243d8871] {
+  .row-search{
     display: inline-flex;
     padding-top: 16px;
 }
@@ -218,7 +218,6 @@ import SearchArea from './SearchArea.vue';
     font-weight: 500;
     font-size: 16px;
     line-height: 24px;
-    color: #fff;
     max-width: calc(100% - 16px);
     overflow: hidden;
     text-overflow: ellipsis;
@@ -240,14 +239,12 @@ import SearchArea from './SearchArea.vue';
     font-weight: 400;
     font-size: 14px;
     line-height: 24px;
-    color: #2d405766;
     cursor:text;
 }
 
   .Icon-MagnifyingGlass{
     width: 24px;
     height: 24px;
-    color:#2d405766;
   }
 
 
@@ -255,6 +252,7 @@ import SearchArea from './SearchArea.vue';
     position: relative;
     overflow-x: auto;
     -ms-overflow-style: none;
+
     scrollbar-width: none
   }
 
@@ -283,7 +281,7 @@ import SearchArea from './SearchArea.vue';
   .block-category .category-item.active {
     color: #D23430;
     border-bottom: 2px solid #D23430;
-    pointer-events: none
+    pointer-events: none;
   }
 
   .block-filter {
@@ -291,12 +289,10 @@ import SearchArea from './SearchArea.vue';
     padding-top: 3px;
   }
 
-  .btn[data-v-filter]{
+  .filter-btn{
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    color: #fff;
-    background-color: #fff3;
     border-radius: 50%;
     width: 24px;
     height: 24px;
@@ -310,12 +306,11 @@ import SearchArea from './SearchArea.vue';
   .Icon-Filter{
     width: 1em;
     height: 1em;
-    color: black;
     fill: currentColor;
   }
 
 @media screen and (min-width: 1200px) {
-  .row-search[data-v-243d8871] {
+  .row-search {
     align-items:center;
     padding-top: 0;
     justify-content: center;
@@ -369,9 +364,9 @@ import SearchArea from './SearchArea.vue';
     padding:0 70px 0 16px
   }
 
-  .block-category .category-item:hover:not(.active) {
-    color:#2d4057cc;
-    border-bottom: 2px solid rgba(54,154,217,.2)
+  .category-item:hover:not(.active) {
+    color: gray;
+    border-bottom: 2px solid #D23430
   }
 
   .block-filter{
