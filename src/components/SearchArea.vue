7854b11e2e8551dcd5071dfb33ed3248273ba5fc
Rod Chang <a0942248868@gmail.com>
2024-11-26 12:19:38 +0800

    調整標籤大小

--- src/components/SearchArea.vue
+++ src/components/SearchArea.vue
@@ -15,13 +15,13 @@ const closeModal = () => {
 
 <template>
     <!-- 區域選擇按鈕 -->
-    <div class="block-area pr-2">
+    <div class="block-area flex justify-center items-center pr-2">
       <button 
-        class="btn btn-sm border-transparent inline-flex items-center justify-between h-4 rounded-3xl cursor-pointer mt-0 mx-0 mb-1 w-22 bg-primary-600
+        class="btn btn-sm border-transparent inline-flex items-center justify-center h-4 rounded-3xl cursor-pointer mt-0 mx-0  w-22 bg-primary-600
         hover:bg-primary-500" 
         onclick="SearchArea.showModal()"
       >
-        <span class="text-gray flex font-medium overflow-hidden text-ellipsis
+        <span class="text-gray flex font-medium overflow-hidden text-ellipsis justify-center items-center
           ">選擇區域
           <ChevronDownIcon class="Icon-SelectArea w-4 h-4 pl-1 text-white text-primary-800"/>
         </span>
