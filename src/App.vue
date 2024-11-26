66bc22bdc61a037a4498756ea3b9817e09eef118
Rod Chang <a0942248868@gmail.com>
2024-11-26 18:04:26 +0800

    移除多餘標籤

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { GlobeAsiaAustraliaIcon } from '@heroicons/vue/24/solid'
-import MapToggle from './components/MapToggle.vue'
-import DetailModal from './components/DetailModal.vue'
-import DetailCarousel from './components/DetailCarousel.vue'
-import PlacesComponent from './components/PlacesComponent.vue'
-import PlacesModal from './components/PlacesModal.vue'
-import MapComponent from './components/MapComponent.vue'
-import SearchBar from './components/SearchBar.vue';
+
 
 
 </script>
