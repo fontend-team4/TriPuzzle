cd5c534738ecbac2b1a1faaa0b166c39994cf04b
XX <kapritmyrtis@gmail.com>
2025-01-01 22:30:24 +0800

    hotfix: 修正共邊功能無法即時更新、行程列表圖片顯示問題 (#114)

--- src/components/ScheduleDetail.vue
+++ src/components/ScheduleDetail.vue
@@ -30,8 +30,6 @@ const groupedPlaces = ref({});
 const API_URL = process.env.VITE_HOST_URL;
 const userId = ref(localStorage.getItem("userId"));
 const token = localStorage.getItem("token");
-const GOOGLE_PHOTO_BASE_URL = 'https://maps.googleapis.com/maps/api/place/photo';
-const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 const activeTab = ref(0);
 const setActiveTab = (index) => {
     activeTab.value = index;
@@ -46,9 +44,7 @@ const getDayInChinese = (date) => {
   return daysInChinese[new Date(date).getDay()];
 };
 
-const getImageUrl = (photoReference) => {
-  return `${GOOGLE_PHOTO_BASE_URL}?maxwidth=400&photoreference=${photoReference}&key=${GOOGLE_API_KEY}`;
-};
+
 
 const formatDate = (dateString) => {
   const options = { hour: '2-digit', minute: '2-digit', hour12: false };
@@ -128,7 +124,7 @@ const drag = ref(false)
           <li class="whitespace-nowrap">
             <a
               @click.prevent="setActiveTab(-1)"
-              class="pb-2 font-medium text-sm hover:text-primary-600"
+              class="pb-2 text-sm font-medium hover:text-primary-600"
               >總覽頁</a
             >
           </li>
@@ -138,7 +134,7 @@ const drag = ref(false)
             class="whitespace-nowrap">
             <a
               @click="setActiveTab(index)"
-              class="pb-2 font-medium text-sm hover:text-primary-600"
+              class="pb-2 text-sm font-medium hover:text-primary-600"
               >第{{ index + 1 }}天</a
             >
           </li>
@@ -155,8 +151,8 @@ const drag = ref(false)
     <div v-for="(places, date, index) in groupedPlaces" :key="date">
       <div v-if="activeTab === index">
         <!-- date -->
-        <div class="pt-5 px-5 mb-3">
-          <div class="date w-36 flex gap-1 cursor-pointer">
+        <div class="px-5 pt-5 mb-3">
+          <div class="flex gap-1 cursor-pointer date w-36">
             <p class="font-medium">{{ formatDateWithoutTime(date) }}週{{ getDayInChinese(date) }}</p>
             <div class="dropdown lg:hidden">
               <div class="w-6 h-6" tabindex="0" role="button">
@@ -186,20 +182,20 @@ const drag = ref(false)
           item-key="id"
         >
           <template #item="{ element }" >
-            <div class="place-transportation container">
+            <div class="container place-transportation">
               <div
-                class="pt-2 px-5 pb-1 overflow-y-hidden overflow-x-hidden bg-white"
+                class="px-5 pt-2 pb-1 overflow-x-hidden overflow-y-hidden bg-white"
               >
                 <div
                   :key="element.id"
-                  class="place w-full bg-gray rounded-xl border border-gray mt-4"
+                  class="w-full mt-4 border place bg-gray rounded-xl border-gray"
                 >
                   <div class="flex p-1">
                     <img
                       class="w-[108px] h-[108px] rounded-xl object-cover"
-                      :src="getImageUrl(element.places.image_url)"
+                      :src="element.places.image_url"
                     />
-                    <div class="w-cal flex justify-between">
+                    <div class="flex justify-between w-cal">
                       <ul class="px-4 flex flex-col gap-0.5 justify-center">
                         <li class="text-sm font-medium text-orange-400">
                           {{ formatDate(element.arrival_time) }}
@@ -209,10 +205,10 @@ const drag = ref(false)
                           {{ `停留 ${formatDuration(element.stay_time)} ` }}
                         </li>
                       </ul>
-                      <div class="dropdown p-1">
+                      <div class="p-1 dropdown">
                         <button
                           role="button"
-                          class="w-5 h-5 rounded-full bg-gray-300 hover:bg-gray-400 text-white relative"
+                          class="relative w-5 h-5 text-white bg-gray-300 rounded-full hover:bg-gray-400"
                         >
                           <EllipsisHorizontalIcon />
                         </button>
@@ -223,7 +219,7 @@ const drag = ref(false)
                         >
                           <li @click="moveDate.showModal()">
                             <a
-                              class="flex items-center gap-1 text-sm px-5 py-2 hover:bg-gray"
+                              class="flex items-center gap-1 px-5 py-2 text-sm hover:bg-gray"
                               href="#"
                             >
                               <span class="inline-block w-6 h-6"
@@ -235,7 +231,7 @@ const drag = ref(false)
                           <MoveToOtherDateModal />
                           <li>
                             <a
-                              class="flex items-center gap-1 text-sm px-5 py-2 hover:bg-gray"
+                              class="flex items-center gap-1 px-5 py-2 text-sm hover:bg-gray"
                               href="#"
                             >
                               <span class="inline-block w-6 h-6"
@@ -246,7 +242,7 @@ const drag = ref(false)
                           </li>
                           <li class="border-t" @click="delete_place.showModal()">
                             <a
-                              class="flex items-center gap-1 text-sm px-5 py-2 hover:bg-gray"
+                              class="flex items-center gap-1 px-5 py-2 text-sm hover:bg-gray"
                               href="#"
                             >
                               <span class="inline-block w-6 h-6"
@@ -263,11 +259,11 @@ const drag = ref(false)
                   <!-- hover:relative feature -->
                   <div class="more lg:hidden">
                     <div
-                      class="h-10 flex justify-between items-center border-t border-white"
+                      class="flex items-center justify-between h-10 border-t border-white"
                     >
                       <ul class="flex gap-4 p-3">
                         <li
-                          class="flex items-center text-gray-500 gap-1 hover:cursor-pointer"
+                          class="flex items-center gap-1 text-gray-500 hover:cursor-pointer"
                           @click="edit_place.showModal()"
                         >
                           <span class="w-3 h-3"><PencilIcon /></span>
@@ -275,13 +271,13 @@ const drag = ref(false)
                         </li>
                         <EditPlaceModal />
                         <li
-                          class="flex items-center text-gray-500 gap-1 hover:cursor-pointer"
+                          class="flex items-center gap-1 text-gray-500 hover:cursor-pointer"
                         >
                           <span class="w-3 h-3"><MagnifyingGlassIcon /></span>
                           <p class="text-xs">周邊</p>
                         </li>
                         <li
-                          class="flex items-center text-gray-500 gap-1 hover:cursor-pointer"
+                          class="flex items-center gap-1 text-gray-500 hover:cursor-pointer"
                           @click="place_note_1.showModal()"
                         >
                           <span class="w-3 h-3"><BookmarkIcon /></span>
@@ -295,13 +291,13 @@ const drag = ref(false)
                           >
                             <form method="dialog">
                               <button
-                                class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                                class="absolute btn btn-sm btn-circle btn-ghost right-2 top-2"
                               >
                                 ✕
                               </button>
                             </form>
                             <h2
-                              class="text-2xl font-medium text-center pt-6 mb-4"
+                              class="pt-6 mb-4 text-2xl font-medium text-center"
                             >
                               景點筆記
                             </h2>
@@ -310,15 +306,15 @@ const drag = ref(false)
                               placeholder="還沒有寫筆記哦"
                             ></textarea>
                             <div
-                              class="w-full flex gap-3 h-20 px-6 py-4 bg-white border-t border-gray fixed bottom-0 right-0"
+                              class="fixed bottom-0 right-0 flex w-full h-20 gap-3 px-6 py-4 bg-white border-t border-gray"
                             >
                               <button
-                                class="w-full h-12 px-5 py-3 border border-primary-600 text-primary-600 text-center rounded-3xl font-medium hover:bg-primary-100"
+                                class="w-full h-12 px-5 py-3 font-medium text-center border border-primary-600 text-primary-600 rounded-3xl hover:bg-primary-100"
                               >
                                 取消
                               </button>
                               <button
-                                class="w-full h-12 px-5 py-3 bg-primary-600 hover:bg-primary-700 text-white text-center rounded-3xl font-medium"
+                                class="w-full h-12 px-5 py-3 font-medium text-center text-white bg-primary-600 hover:bg-primary-700 rounded-3xl"
                               >
                                 儲存
                               </button>
@@ -326,7 +322,7 @@ const drag = ref(false)
                           </div>
                           <form method="dialog" class="modal-backdrop">
                             <button
-                              class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                              class="absolute btn btn-sm btn-circle btn-ghost right-2 top-2"
                             >
                               ✕
                             </button>
@@ -335,7 +331,7 @@ const drag = ref(false)
                         </dialog>
                       </ul>
                       <span
-                        class="w-10 h-10 text-gray-500 p-3 border-l border-white hover:cursor-pointer"
+                        class="w-10 h-10 p-3 text-gray-500 border-l border-white hover:cursor-pointer"
                         ><MapPinIcon
                       /></span>
                     </div>
@@ -344,7 +340,7 @@ const drag = ref(false)
               </div>
               <p
                 v-if="element !== place[place.length - 1]"
-                class="w-full ml-3 py-4 ps-3 border-l border-dashed border-gray hover:cursor-pointer"
+                class="w-full py-4 ml-3 border-l border-dashed ps-3 border-gray hover:cursor-pointer"
                 @click="transportationToggle"
               >
                 {{ `自訂交通 ${element.transhour} 分` }}
