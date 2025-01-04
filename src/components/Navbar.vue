42a3f8b6da62247834370cf1cf88024a962ba0d2
Emma Huang <huiwenhuang1998@gmail.com>
2025-01-04 14:10:38 +0800

    feat: 新增首頁、會員頁全頁式 Loading 樣式

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { defineEmits, ref } from "vue"
+import { useRouter } from "vue-router"
 import { PuzzlePieceIcon, Bars3Icon } from "@heroicons/vue/20/solid"
+import { useLoadingStore } from "@/stores/loading"
 
+const loadingStore = useLoadingStore()
 const isOpen = ref(false)
+const router = useRouter()
+
 const openDropdown = () => {
   isOpen.value = !isOpen.value
 }
@@ -10,9 +15,19 @@ const emit = defineEmits(["clickTab"])
 const clickTab = (sectionName) => {
   emit("clickTab", sectionName)
 }
+const goToPlanner = () => {
+  loadingStore.showLoading()
+  router.push("/planner")
+  loadingStore.hideLoading()
+}
 </script>
 
 <template>
+  <LoadingOverlay :active="loadingStore.isLoading">
+    <div class="loadingio-spinner-ellipsis-nq4q5u6dq7r"><div class="ldio-x2uulkbinbj">
+    <div></div><div></div><div></div><div></div><div></div>
+    </div></div>
+  </LoadingOverlay>
   <nav
     class="border-b border-neutral-200 bg-white z-10 w-screen fixed top-0 md:hidden"
   >
@@ -21,13 +36,13 @@ const clickTab = (sectionName) => {
         <Bars3Icon class="w-6 h-6 text-neutral-800" @click="openDropdown" />
         <img src="@/assets/svg/Logo.svg" class="ml-3 w-auto h-8" />
       </div>
-      <RouterLink
-        to="/planner"
+      <button
+        @click="goToPlanner"
         class="flex gap-1 items-center bg-primary-800 px-2 py-[6px] text-xs text-neutral-50 rounded-full hover:bg-primary-600"
       >
         <PuzzlePieceIcon class="w-4 h-4 text-neutral-50" />
         拼湊旅圖
-      </RouterLink>
+      </button>
     </div>
     <div
       :class="isOpen ? 'block' : 'hidden'"
@@ -93,13 +108,76 @@ const clickTab = (sectionName) => {
           註冊會員
         </li>
       </ul>
-      <RouterLink
-        to="/planner"
+      <button
+        @click="goToPlanner"
         class="flex gap-1 items-center bg-primary-800 px-5 py-2 ml-4 text-base text-neutral-50 rounded-full hover:bg-primary-600"
       >
         <PuzzlePieceIcon class="w-5 h-5 text-neutral-50" />
         拼湊旅圖
-      </RouterLink>
+      </button>
     </div>
   </nav>
 </template>
+
+<style>
+@keyframes ldio-x2uulkbinbj {
+  0% { transform: translate(14px,82px) scale(0); }
+  25% { transform: translate(14px,82px) scale(0); }
+  50% { transform: translate(14px,82px) scale(1); }
+  75% { transform: translate(82px,82px) scale(1); }
+  100% { transform: translate(150px,82px) scale(1); }
+}
+@keyframes ldio-x2uulkbinbj-r {
+  0% { transform: translate(150px,82px) scale(1); }
+  100% { transform: translate(150px,82px) scale(0); }
+}
+@keyframes ldio-x2uulkbinbj-c {
+  0% { background: #b12824 }
+  25% { background: #fbcecd }
+  50% { background: #f17b78 }
+  75% { background: #d23430 }
+  100% { background: #b12824 }
+}
+.ldio-x2uulkbinbj div {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  transform: translate(82px,82px) scale(1);
+  background: #b12824;
+  animation: ldio-x2uulkbinbj 2.5s infinite cubic-bezier(0,0.5,0.5,1);
+}
+.ldio-x2uulkbinbj div:nth-child(1) {
+  background: #d23430;
+  transform: translate(150px,82px) scale(1);
+  animation: ldio-x2uulkbinbj-r 0.625s infinite cubic-bezier(0,0.5,0.5,1), ldio-x2uulkbinbj-c 2.5s infinite step-start;
+}.ldio-x2uulkbinbj div:nth-child(2) {
+  animation-delay: -0.625s;
+  background: #b12824;
+}.ldio-x2uulkbinbj div:nth-child(3) {
+  animation-delay: -1.25s;
+  background: #d23430;
+}.ldio-x2uulkbinbj div:nth-child(4) {
+  animation-delay: -1.875s;
+  background: #f17b78;
+}.ldio-x2uulkbinbj div:nth-child(5) {
+  animation-delay: -2.5s;
+  background: #fbcecd;
+}
+.loadingio-spinner-ellipsis-nq4q5u6dq7r {
+  width: 200px;
+  height: 200px;
+  display: inline-block;
+  overflow: hidden;
+  background: none;
+}
+.ldio-x2uulkbinbj {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: translateZ(0) scale(1);
+  backface-visibility: hidden;
+  transform-origin: 0 0; /* see note above */
+}
+.ldio-x2uulkbinbj div { box-sizing: content-box; }
+</style>
