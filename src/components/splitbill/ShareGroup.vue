5215c478472b8bfdf13a61b631e6864723fb2223
Emma Huang <huiwenhuang1998@gmail.com>
2025-01-08 12:25:07 +0800

    feat: 新增 Loading 樣式與彈窗提示更新。

--- src/components/splitbill/ShareGroup.vue
+++ src/components/splitbill/ShareGroup.vue
@@ -2,7 +2,10 @@
 import { ref, onMounted, inject } from 'vue';
 import axios from 'axios';
 import defaultUserImage from '/images/cat-2.png';
+import '@/assets/loading.css';
+import { useLoadingStore } from '@/stores/loading';
 
+const loadingStore = useLoadingStore();
 const token = localStorage.getItem('token');
 const API_URL = process.env.VITE_HOST_URL;
 
@@ -10,11 +13,25 @@ const groupMembers = inject('groupMembers'); // 從父組件獲取成員列表
 const scheduleId = inject('scheduleId'); // 從父組件獲取 scheduleId
 
 onMounted(async () => {
-  console.log('mounted');
+  loadingStore.showLoading();
+  setTimeout(() => {
+    loadingStore.hideLoading();
+  }, 3000);
 });
 </script>
 
 <template>
+  <LoadingOverlay :active="loadingStore.isLoading">
+  <div class="loadingio-spinner-ellipsis-nq4q5u6dq7r">
+    <div class="ldio-x2uulkbinbj">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  </LoadingOverlay>
   <div class="p-4 bg-white shadow-md rounded-b-lg rounded-l-lg md:rounded-lg">
     <h2 class="text-xl font-bold text-primary-500 mb-2">
       群組成員 :【{{ groupMembers.title }}】
@@ -25,7 +42,7 @@ onMounted(async () => {
         <img
           :src="groupMembers.creator?.profile_pic_url || defaultUserImage"
           alt="Logo"
-          class="w-8 mr-2 rounded-full"
+          class="w-10 h-10 object-cover rounded-full mr-2"
         />
         {{ groupMembers.creator?.name }} - {{ groupMembers.creator?.email }}
       </li>
@@ -36,9 +53,9 @@ onMounted(async () => {
         class="py-2 text-gray-800 flex flex-row items-center"
       >
         <img
-          :src="groupMembers.creator?.profile_pic_url || defaultUserImage"
+          :src="groupMembers.sharedUsers?.profile_pic_url || defaultUserImage"
           alt="Logo"
-          class="w-8 mr-2 rounded-full"
+          class="w-10 h-10 object-cover rounded-full mr-2"
         />
         {{ sharedUsers.name }} -
         <span class="text-gray-500">{{ sharedUsers.email }}</span>
