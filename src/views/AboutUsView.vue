d3f8aa11b6c6410f4ede3847eba28f7de2a2ef1a
XX <kapritmyrtis@gmail.com>
2025-01-09 16:30:28 +0800

    fix: 修改about資訊

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -5,29 +5,26 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
 
 <template>
   <nav
-    class="border-b-2 border-neutral-200 bg-white z-10 sticky top-0  md:block"
+    class="sticky top-0 z-10 bg-white border-b-2 border-neutral-200 md:block"
   >
-    <div
-      class="w-screen py-4 mx-auto flex justify-between items-center"
-    >
-    <router-link to="/">
-      <img
-        src="@/assets/svg/Logo.svg"
-        class="flex-none cursor-pointer ml-8"
-        @click="clickTab('top')"
-      />
-    </router-link>
-    <router-link to="/planner">
-      <button
-        class="flex mr-8 gap-1 items-center bg-primary-800 px-5 py-2 ml-4 text-base text-neutral-50 rounded-full hover:bg-primary-600"
-      >
-        <PuzzlePieceIcon class="w-5 h-5 text-neutral-50" />
-        拼湊旅圖
-      </button>
-    </router-link>
+    <div class="flex items-center justify-between w-screen py-4 mx-auto">
+      <router-link to="/">
+        <img
+          src="@/assets/svg/Logo.svg"
+          class="flex-none ml-8 cursor-pointer"
+          @click="clickTab('top')"
+        />
+      </router-link>
+      <router-link to="/planner">
+        <button
+          class="flex items-center gap-1 px-5 py-2 ml-4 mr-8 text-base rounded-full bg-primary-800 text-neutral-50 hover:bg-primary-600"
+        >
+          <PuzzlePieceIcon class="w-5 h-5 text-neutral-50" />
+          拼湊旅圖
+        </button>
+      </router-link>
     </div>
   </nav>
-
 
   <div class="bg-primary-200">
     <img src="@/assets/images/aboutus_WB.png" class="hidden lg:block" />
@@ -43,7 +40,7 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
     <div
       class="hidden md:grid lg:grid grid-cols-12 place-content-around bg-primary-900 h-[600px]"
     >
-      <div class="col-start-3 col-span-3 self-center">
+      <div class="self-center col-span-3 col-start-3">
         <h2 class="text-secondary-500 font-bold text-xl mb-[10%]">
           為什麼會有旅圖？
         </h2>
@@ -63,9 +60,9 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
         class="w-full lg:col-start-8 md:col-start-8 lg:col-span-3 md:col-span-4 mt-8 self-center lg:max-w-[432px]"
       />
     </div>
-    <div class="md:hidden lg:hidden bg-primary-900 p-10">
-      <div class="z-40 pr-2 pl-2 pt-2">
-        <h2 class="text-secondary-500 font-bold text-xl">為什麼會有旅圖？</h2>
+    <div class="p-10 md:hidden lg:hidden bg-primary-900">
+      <div class="z-40 pt-2 pl-2 pr-2">
+        <h2 class="text-xl font-bold text-secondary-500">為什麼會有旅圖？</h2>
         <p class="text-white h-[80%] py-2 min-w-44">
           我們是來自商研院前端應用開發工程師實戰養成班的團隊，成員來自不同背景，共同擁有旅遊熱情。
           <br />
@@ -83,18 +80,16 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
       />
     </div>
 
-    <div
-      class="h-[5vh] bg-primary-200 w-screen md:h-[5vh] lg:h-[15vh]"
-    ></div>
+    <div class="h-[5vh] bg-primary-200 w-screen md:h-[5vh] lg:h-[15vh]"></div>
     <h2
       class="lg:hidden text-primary-700 text-4xl font-bold h-[12vh] flex items-center justify-center pt-6 w-screen"
     >
       團隊成員
     </h2>
-    <div class="w-screen flex">
-      <section class="grid grid-cols-12 mx-auto place-content-around py-10">
+    <div class="flex w-screen">
+      <section class="grid grid-cols-12 py-10 mx-auto place-content-around">
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-4"
         >
           <img
             src="@/assets/images/member1.jpg"
@@ -128,12 +123,12 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           </div>
         </div>
         <div
-          class="hidden col-span-8 col-start-5 place-content-around lg:flex items-center justify-center"
+          class="items-center justify-center hidden col-span-8 col-start-5 place-content-around lg:flex"
         >
-          <h2 class="text-primary-700 text-6xl font-bold">團隊成員</h2>
+          <h2 class="text-6xl font-bold text-primary-700">團隊成員</h2>
         </div>
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-7 md:col-span-6 lg:col-start-1 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-7 md:col-span-6 lg:col-start-1 lg:col-span-4"
         >
           <img
             src="@/assets/images/member2.jpg"
@@ -147,14 +142,14 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
             <p>專案 & 後端資料庫規劃管理</p>
           </span>
           <div class="flex">
-              <img
-                src="@/assets/images/github.png"
-                class="w-[28px] group-hover:hidden"
-              />
-              <img
-                src="@/assets/images/github_grey.png"
-                class="w-[28px] hidden group-hover:block"
-              />
+            <img
+              src="@/assets/images/github.png"
+              class="w-[28px] group-hover:hidden"
+            />
+            <img
+              src="@/assets/images/github_grey.png"
+              class="w-[28px] hidden group-hover:block"
+            />
             <a
               href="mailto:huiwenhuang1998@gmail.com"
               class="text-zinc-800 hover:text-zinc-500"
@@ -164,7 +159,7 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           </div>
         </div>
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-1 md:col-span-6 lg:col-start-5 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-1 md:col-span-6 lg:col-start-5 lg:col-span-4"
         >
           <img
             src="@/assets/images/member3.jpg"
@@ -172,20 +167,22 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           />
           <h3 class="text-2xl text-zinc-800">張綺恩</h3>
           <span class="flex flex-col items-center text-zinc-500">
-            <p>Google Map 、瀑布流渲染</p>
+            <p>地圖、瀑布流同步渲染</p>
             <p>共同編輯功能</p>
+            <p>Google Maps Marker設置</p>
             <p>景點詳細資料渲染</p>
+            <p>建立總行程後端API</p>
           </span>
           <div class="flex">
-              <img
-                src="@/assets/images/github.png"
-                class="w-[28px] group-hover:hidden"
-              />
-              <img
-                src="@/assets/images/github_grey.png"
-                class="w-[28px] hidden group-hover:block"
-              />
-              <a
+            <img
+              src="@/assets/images/github.png"
+              class="w-[28px] group-hover:hidden"
+            />
+            <img
+              src="@/assets/images/github_grey.png"
+              class="w-[28px] hidden group-hover:block"
+            />
+            <a
               href="mailto:lemonmaggiex@gmail.com"
               class="text-zinc-800 hover:text-zinc-500"
             >
@@ -194,7 +191,7 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           </div>
         </div>
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-7 md:col-span-6 lg:col-start-9 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-7 md:col-span-6 lg:col-start-9 lg:col-span-4"
         >
           <img
             src="@/assets/images/member4.jpg"
@@ -207,14 +204,14 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
             <p>首頁製作</p>
           </span>
           <div class="flex">
-              <img
-                src="@/assets/images/github.png"
-                class="w-[28px] group-hover:hidden"
-              />
-              <img
-                src="@/assets/images/github_grey.png"
-                class="w-[28px] hidden group-hover:block"
-              />
+            <img
+              src="@/assets/images/github.png"
+              class="w-[28px] group-hover:hidden"
+            />
+            <img
+              src="@/assets/images/github_grey.png"
+              class="w-[28px] hidden group-hover:block"
+            />
             <a
               href="mailto:timothy9am@gmail.com"
               class="text-zinc-800 hover:text-zinc-500"
@@ -224,7 +221,7 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           </div>
         </div>
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-1 md:col-span-6 lg:col-start-1 lg:col-span-4"
         >
           <img
             src="@/assets/images/member5.jpg"
@@ -256,7 +253,7 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           </div>
         </div>
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-7 md:col-span-6 lg:col-start-5 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-7 md:col-span-6 lg:col-start-5 lg:col-span-4"
         >
           <img
             src="@/assets/images/member6.jpg"
@@ -288,7 +285,7 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
           </div>
         </div>
         <div
-          class="mx-4 mb-6 bg-primary-100 flex flex-col col-start-1 col-span-12 md:col-start-7 md:col-span-6 lg:col-start-9 lg:col-span-4 items-center py-8 px-20 gap-8"
+          class="flex flex-col items-center col-span-12 col-start-1 gap-8 px-20 py-8 mx-4 mb-6 bg-primary-100 md:col-start-7 md:col-span-6 lg:col-start-9 lg:col-span-4"
         >
           <img
             src="@/assets/images/member7.jpg"
@@ -324,31 +321,35 @@ import { PuzzlePieceIcon } from '@heroicons/vue/24/solid';
     <div
       class="h-[5vh] md:block lg:block bg-primary-200 w-screen md:h-[5vh] lg:h-[15vh]"
     ></div>
-    <footer class="bg-gradient-to-t from-primary-50 pt-5 md:pt-10">
-    <div class="w-full px-4 mx-auto relative md:w-9/12 md:max-w-7xl md:px-0">
-      <div class="flex justify-center items-center gap-3 mb-5 md:flex-col">
-        <h1
-          class="text-lg font-bold text-neutral-800 sm:text-xl md:text-3xl md:mb-10 text-center"
-        >
-          <span class="text-primary-800">旅圖</span>
-          ，旅程從這裡開始<br />讓我們一起拼湊出你的旅途
-        </h1>
+    <footer class="pt-5 bg-gradient-to-t from-primary-50 md:pt-10">
+      <div class="relative w-full px-4 mx-auto md:w-9/12 md:max-w-7xl md:px-0">
+        <div class="flex items-center justify-center gap-3 mb-5 md:flex-col">
+          <h1
+            class="text-lg font-bold text-center text-neutral-800 sm:text-xl md:text-3xl md:mb-10"
+          >
+            <span class="text-primary-800">旅圖</span>
+            ，旅程從這裡開始
+            <br />
+            讓我們一起拼湊出你的旅途
+          </h1>
+        </div>
+        <div class="py-4 border-t-2 border-neutral-200 md:py-7">
+          <p
+            class="text-primary-800 text-[9px] text-center font-bold md:text-sm"
+          >
+            ©2024 旅圖 TriPuzzle, all rights reserved. Powered by NO.4
+            Corporation.
+          </p>
+        </div>
+        <img
+          src="@/assets/svg/FooterIllustration1.svg"
+          class="scale-75 absolute top-32 -left-[150px] hidden lg:block xl:scale-100 xl:top-[100px] xl:-left-32"
+        />
+        <img
+          src="@/assets/svg/FooterIllustration2.svg"
+          class="scale-75 absolute top-14 -right-[150px] hidden lg:block xl:scale-100 xl:top-5 xl:-right-[120px]"
+        />
       </div>
-      <div class="py-4 border-t-2 border-neutral-200 md:py-7">
-        <p class="text-primary-800 text-[9px] text-center font-bold md:text-sm">
-          ©2024 旅圖 TriPuzzle, all rights reserved. Powered by NO.4
-          Corporation.
-        </p>
-      </div>
-      <img
-        src="@/assets/svg/FooterIllustration1.svg"
-        class="scale-75 absolute top-32 -left-[150px] hidden lg:block xl:scale-100 xl:top-[100px] xl:-left-32"
-      />
-      <img
-        src="@/assets/svg/FooterIllustration2.svg"
-        class="scale-75 absolute top-14 -right-[150px] hidden lg:block xl:scale-100 xl:top-5 xl:-right-[120px]"
-      />
-    </div>
-  </footer>
+    </footer>
   </div>
 </template>
